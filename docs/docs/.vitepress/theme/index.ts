1d4f6157eca845b6014c182d90f1d6c26470d0e6
jerrywu <jerrywu001@gmail.com>
2024-01-30 11:21:55 +0800

    docs: update sponsor

--- docs/docs/.vitepress/theme/index.ts
+++ docs/docs/.vitepress/theme/index.ts
@@ -1,5 +1,7 @@
 import DefaultTheme from 'vitepress/theme';
 // @ts-ignore
+import Sponsors from '../components/sponsor.vue';
+// @ts-ignore
 import CodePanel from '../../components/CodePanel.vue';
 // @ts-ignore
 import MySandbox from '../../components/MySandbox.vue';
@@ -7,12 +9,18 @@ import MySandbox from '../../components/MySandbox.vue';
 import OptsSandbox from '../../components/OptsSandbox.vue';
 // @ts-ignore
 import Sandbox from '../../../../src/components/Sandbox.vue';
+import { h } from 'vue';
 
 // for build test
 // import { Sandbox } from '../../../../dist/esm';
 
 export default {
   ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'home-features-after': () => h(Sponsors),
+    })
+  },
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx);
     ctx.app.component('Sandbox', Sandbox);
